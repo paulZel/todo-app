a66bddb78e93cd281092e1b703d1bd73695a0e4b
Paul <paul99zel@ukr.net>
2023-08-14 15:50:59 +0300

    Implement the Edit button

--- gui.py
+++ gui.py
@@ -4,9 +4,13 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo") #usuall empty
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+
+edit_button = sg.Button("Edit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20)) #layout expects a list of lists double breckets
 
 # each row in new list
@@ -14,12 +18,25 @@ while True:
     event, values = window.read() #this method will display window on the screen
     print(event)
     print(values)
+    print(values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0]) # 0 because it's the only item of this list
         case sg.WIN_CLOSED:
             break
 
